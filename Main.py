2a3f985b77e08c95922e686653a3abaca724715b
Maksim <Zhegmaks@gmail.com>
2024-07-18 19:05:54 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,9 +5,6 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import asyncio
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import aiohttp
-from datetime import datetime
-import time
 
 storage = MemoryStorage()
 
@@ -21,9 +18,9 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=storage)
 scheduler = AsyncIOScheduler()
 url_list = []
-user_list = [1000337173, 316398758]  # 
-TIMEOUT_PING = 10  # 
-INTERVAL_MINUTES = 5  # 
+user_list = [316398758]  
+TIMEOUT_PING = 10  
+INTERVAL_MINUTES = 5
 
 @dp.message_handler(commands=['sendlist'])
 async def request_file(message: types.Message):
@@ -34,25 +31,27 @@ async def request_file(message: types.Message):
     else:
         await message.reply("Некорректный пароль")
 
+
 @dp.message_handler(state=Form.file_id, content_types=types.ContentTypes.DOCUMENT)
 async def handle_file(message: types.Message, state: Form):
     document_id = message.document.file_id
     file_info = await bot.get_file(document_id)
     file = await bot.download_file(file_info.file_path)
     with open('list.txt', 'wb') as f:
-        f.write(file.getvalue())
+        f.write(file.read())
     with open('list.txt', 'r') as f:
         global url_list
-        url_list = [line.split(' - ')[0] for line in f.read().splitlines()]
+        url_list = [line.strip() for line in f.readlines()]
     scheduler.remove_all_jobs()
     scheduler.add_job(check_urls, 'interval', minutes=INTERVAL_MINUTES)
-    scheduler.add_job(check_urls_hourly, trigger='cron', minute='0')
+    scheduler.add_job(check_urls_hourly, 'cron', hour='8', minute='0')
     await message.reply("Список ссылок обновлен и проверка инициирована")
     await state.finish()
 
+
 @dp.message_handler(lambda message: message.text != PASSWORD, state=Form.file_id)
 async def wrong_password(message: types.Message, state: Form):
-    await message.reply("Некорректный пароль")
+    await message.reply("Введите корректный пароль")
     await state.finish()
 
 @dp.message_handler(commands=['getlist'])
@@ -64,55 +63,57 @@ async def get_list(message: types.Message):
         await message.reply("Некорректный пароль")
 
 async def check_urls():
-    for url in url_list:
+    for line in url_list:
+        parts = line.split(' - ')
+        url = parts[-1]
+        name = ' - '.join(parts[:-1])
         try:
             response = requests.get(url)
             response.raise_for_status()
-            mark_url(url, 'available')
+            mark_url(name, 'available', url)
             await asyncio.sleep(TIMEOUT_PING)
         except requests.RequestException as e:
             print(f"Error fetching URL {url}: {e}")
-            if not is_marked_unavailable(url):
+            if not is_marked_unavailable(name):
                 for user in user_list:
-                    await bot.send_message(user, f"{url} - приложение недоступно в сторе")
+                    await bot.send_message(user, f"{name} - {url} приложение недоступно в сторе")
                 await asyncio.sleep(TIMEOUT_PING)
-                mark_url(url, 'unavailable')
+                mark_url(name, 'unavailable', url)
 
 async def check_urls_hourly():
-    for url in url_list:
+    for line in url_list:
+        parts = line.split(' - ')
+        url = parts[-1]
+        name = ' - '.join(parts[:-1])
         try:
             response = requests.get(url)
             response.raise_for_status()
             for user in user_list:
-                await bot.send_message(user, f"Чекер работает корректно")
-            mark_url(url, 'available')
+                await bot.send_message(user, "Чекер работает корректно")
+            mark_url(name, 'available', url)
             break  
-
         except requests.RequestException as e:
             print(f"Error fetching URL {url}: {e}")
-            if not is_marked_unavailable(url):
+            if not is_marked_unavailable(name):
                 for user in user_list:
-                    await bot.send_message(user, f"{url} - приложение недоступно в сторе")
+                    await bot.send_message(user, f"{name} - приложение недоступно в сторе")
                 await asyncio.sleep(TIMEOUT_PING)
-                mark_url(url, 'unavailable')
+                mark_url(name, 'unavailable', url)
 
-def mark_url(url, status):
+def mark_url(name, status, url):
     with open('list.txt', 'r+') as f:
         lines = f.readlines()
         f.seek(0)
-        for line in lines:
-            if line.startswith(url):
-                f.write(f"{url} - {status}\n")
-            else:
-                f.write(line)
+        for i, line in enumerate(lines):
+            if url in line:
+                lines[i] = f"{name} - {status} - {url}\n"
+        f.writelines(lines)
         f.truncate()
 
-def is_marked_unavailable(url):
+
+def is_marked_unavailable(name):
     with open('list.txt', 'r') as f:
-        for line in f:
-            if line.startswith(url) and 'unavailable' in line:
-                return True
-    return False
+        return any(f"{name} - unavailable" in line for line in f)
 
 
 if __name__ == '__main__':
